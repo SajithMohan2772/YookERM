7ddcd237e7341b07c2836e281b9a9eee4f8527a6
SajithMohan2772 <sajith2772@gmail.com>
2023-12-16 00:15:31 +0530

    3rd commit

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,10 +1,46 @@
-const registerUser =  async (request, response) => {
-    if (!request.body.email){
-        response.status(400);
-        throw new Error("Please add an email");
-    }
-    response.send('Register User');
-};
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser =  asyncHandler(async (request, response) => {
+        const {name, email, password} = request.body
+
+        // validation
+        if (!name || !email || !password ){
+            response.status(400)
+            throw new Error('Plese fill in all required fields')
+        }
+
+        if (password.length < 6){
+            response.status(400)
+            throw new Error('Password must be up to 6 characters')
+        }
+
+        // check if user email already exists
+        const userExists = await User.findOne({email})
+
+        if (userExists) {
+            response.status(400)
+            throw new Error('Email is already been registered') 
+        }
+
+        // create new user 
+        const user = await User.create({
+            name, 
+            email, 
+            password
+        })
+
+        if (user) {
+            const {_id, name, email, photo, phone, bio } = user
+            response.status(201).json({
+                _id, name, email, photo, phone, bio 
+            });
+        }  else {
+                response.status(400)
+                throw new Error("Invalid user data")
+            }
+
+});
 
 module.exports ={
     registerUser,
--- Backend/middleware/errorMiddleware.js
+++ Backend/middleware/errorMiddleware.js
@@ -1,11 +1,11 @@
 const errorHandler= (error , request, response, next) => {
-    const statusCode = response.statusCode ?response.statusCode
+    const statusCode = response.statusCode ? response.statusCode
     : 500;
     response.status(statusCode);
 
     response.json({
         message: error.message,
-        stack : process.env.NODE_ENV === "development" ?
+        stacks: process.env.NODE_ENV === "development" ?
         error.stack : null,
     });
 

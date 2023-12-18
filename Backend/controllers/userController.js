40509ec5275b6e0b3559b6d774a1778fd14826c6
SajithMohan2772 <sajith2772@gmail.com>
2023-12-18 14:17:59 +0530

    5.1 commit

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -49,7 +49,7 @@ const registerUser =  asyncHandler(async (req, res) => {
         });
 
         if (user) {
-            const {_id, name, email, photo, phone, bio } = user
+            const {_id, name, email, photo, phone, bio, token } = user
             res.status(201).json({
                 _id, name, email, photo, phone, bio, token,
             });
@@ -60,6 +60,37 @@ const registerUser =  asyncHandler(async (req, res) => {
 
 });
 
+
+const getUserByID = asyncHandler(async (req,res) => {
+    const userId = req.params.id;
+
+    try {
+        // Fetch the user from the database using the userId
+        const user = await User.findById(userId);
+    
+    if (!user) {
+        // If user not found, return a 404 status and a corresponding message
+        res.status(404).json({ message: 'User not found' });
+    } else {
+        // If user found, return the user data
+        res.status(200).json(user);
+    }
+    } catch (error) {
+    // If an error occurs during database query or processing, return a 500 status and the error message
+    res.status(500).json({ message: error.message });
+    }
+});
+
+const getAllUsers = asyncHandler(async (req,res) => {
+    try {
+        const users = await User.find(); // Fetch all users from the database
+        res.status(200).json(users); // Return users as JSON response
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+// 
 // LOgin user
 const loginUser = asyncHandler(async (req,res) => {
 
@@ -68,12 +99,12 @@ const loginUser = asyncHandler(async (req,res) => {
     // Validation request
     if (!email || !password) {
         res.status(400);
-        throw new Error("please email and password");
+        throw new Error("please add email and password");
     }
     
     // Check if user exist
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({email})
 
     if (!user) {
         res.status(400);
@@ -82,10 +113,22 @@ const loginUser = asyncHandler(async (req,res) => {
 
     // User exists, check if password is correct
 
-    const passwordIsCorrect = await bcrypt.compare(password, user.password)
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+    if (user && passwordIsCorrect) {
+        const {_id, name, email, photo, phone, bio } = user
+        res.status(200).json({
+            _id, name, email, photo, phone, bio, token,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid email or password");
+    }
 });
 
 module.exports ={
     registerUser,
     loginUser,
+    getUserByID,
+    getAllUsers
 };
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require ('express');
-const { registerUser, loginUser } = require('../controllers/userController')
+const { registerUser, loginUser, getUserByID, getAllUsers } = require('../controllers/userController')
 const router = express.Router();
 
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get("/:id", getUserByID);
+router.get("/", getAllUsers);
 
 module.exports = router;

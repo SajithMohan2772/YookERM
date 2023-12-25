92fce402ab106e9403c683f6336cbe90b772681a
SajithMohan2772 <sajith2772@gmail.com>
2023-12-25 17:06:20 +0530

    commit

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -274,7 +274,33 @@ const message = `
 
 // Reset password
 const resetPassword = asyncHandler(async (req, res) => {
-res.send("reset password")
+    const { password } = req.body;
+    const { resetToken } = req.params;
+
+    // Hash token, then compare to Token in DB
+    const hashedToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+    // fIND tOKEN in DB
+    const userToken = await Token.findOne({
+    token: hashedToken,
+    expiresAt: { $gt: Date.now() },
+    });
+
+    if (!userToken) {
+    res.status(404);
+    throw new Error("Invalid or Expired Token");
+    }
+
+    // Find user
+    const user = await User.findOne({ _id: userToken.userId });
+    user.password = password;
+    await user.save();
+    res.status(200).json({
+    message: "Password Reset Successful, Please Login",
+    });
 });
 
 module.exports ={
--- Backend/server.js
+++ Backend/server.js
@@ -40,14 +40,24 @@ const PORT = process.env.PORT || 5000;
 //         })
 //         .catch((error) => console.log(error))
 
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server Running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => console.error(error));
+
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server Running on port ${PORT}`);
-    });
-  })
-  .catch((error) => console.error(error));
+.connect(process.env.MONGO_URI)
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`);
+  });
+})
+.catch((error) => console.error(error));
